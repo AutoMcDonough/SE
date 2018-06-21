db624c1c8e8609adcbd219a959fa6b39fd77e963
AutoMcDonough <dave.m.mcdonough@gmail.com>
2018-06-20 21:13:45 -0400

    Delete settings.cs

--- Data/Scripts/ttdtwm_ttrcwm_DS_test/settings.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ttrcrwm
-{
-    static class settings
-    {
-        public const float              TORQUE_MULTIPLIER = 5.0f, MANUAL_INPUT_SENSITIVITY = 2.0f;
-        public static readonly string[] RCS_subtypes      = { "RCSLBST", "RCSSBST", "RCSIonLBST", "RCSIonSBST", "MA_thruster_gimbal" };
-    }
-}
