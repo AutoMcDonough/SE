b076c04f8f4eb7ec789e925bf6432a8004a0d7bc
AutoMcDonough <dave.m.mcdonough@gmail.com>
2018-06-20 21:14:37 -0400

    Add files via upload

---
+++ MA_Thrusters/Data/Scripts/ttdtwm_ttrcwm_DS_test/settings.cs
@@ -0,0 +1,8 @@
+﻿namespace ttrcrwm
+{
+    static class settings
+    {
+        public const float              TORQUE_MULTIPLIER = 5.0f, MANUAL_INPUT_SENSITIVITY = 2.0f;
+        public static readonly string[] RCS_subtypes      = { "RCSLBST", "RCSSBST", "RCSIonLBST", "RCSIonSBST", "MA_thruster_gimbal" };
+    }
+}
